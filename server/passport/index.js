5aa9eebe247caf49cd771198c7d2423aa10153a4
InTaek <dlsxor21c@naver.com>
2020-10-13 21:22:54 +0900

    Set code for passport

---
+++ server/passport/index.js
@@ -0,0 +1,19 @@
+const local = require("./localStrategy");
+const { User } = require("../models");
+
+module.exports = (passport) => {
+  passport.serializeUser((user, done) => {
+    done(null, user.id);
+  });
+
+  passport.deserializeUser(async (id, done) => {
+    try {
+      const user = await User.findOne({ where: { id } });
+      done(null, user);
+    } catch (error) {
+      done(error);
+    }
+  });
+
+  local(passport);
+};
