315dfbada7dc73d8bb6243e15321eaa3569c2ece
InTaek <dlsxor21c@naver.com>
2020-10-13 20:45:38 +0900

    Make user table

---
+++ server/models/user.js
@@ -0,0 +1,35 @@
+module.exports = (sequelize, DataTypes) =>
+  sequelize.define(
+    "user",
+    {
+      name: {
+        type: DataTypes.STRING(40),
+        allowNull: false,
+        unique: true,
+      },
+      password: {
+        type: DataTypes.STRING(100),
+        allowNull: false,
+      },
+      level: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+      },
+      studyList: {
+        type: DataTypes.STRING(100),
+        allowNull: true,
+      },
+      codingList: {
+        type: DataTypes.STRING(100),
+        allowNull: true,
+      },
+      dockerName: {
+        type: DataTypes.STRING(100),
+        allowNull: true,
+      },
+    },
+    {
+      timestamps: true,
+      paranoid: true,
+    }
+  );
