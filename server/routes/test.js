df8c4e5bc3b00fe795d5ca66b32d83e67c0b13bd
InTaek <dlsxor21c@naver.com>
2020-10-20 17:32:45 +0900

    Ready to communicate

--- server/routes/test.js
+++ server/routes/test.js
@@ -5,7 +5,7 @@ const router = express.Router();
 
 router.get("/", isNotLoggedIn, (req, res, next) => {
   console.log("check");
-  res.render("main", {});
+  res.send("안녕");
 });
 
 module.exports = router;
