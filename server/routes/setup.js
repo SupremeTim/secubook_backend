84913153693f85c7bc19531355048b0713140955
InTaek <dlsxor21c@naver.com>
2020-10-27 16:10:47 +0900

    feat: 더미데이터 추가용 라우터

---
+++ server/routes/setup.js
@@ -0,0 +1,50 @@
+const express = require("express");
+const { Education } = require("../models");
+
+const router = express.Router();
+
+router.get("/", async (req, res, next) => {
+  try {
+    Education.create({
+      title: "SQL-Injection",
+      category: 0,
+      page: 0,
+      content: "test",
+      image:
+        "https://secubook-img-data.s3.ap-northeast-2.amazonaws.com/Unknown.jpeg",
+    });
+
+    Education.create({
+      title: "SQL-Injection",
+      category: 0,
+      page: 1,
+      content: "test입니다.",
+      image:
+        "https://secubook-img-data.s3.ap-northeast-2.amazonaws.com/Unknown.jpeg",
+    });
+
+    res.send({ testMessage: "더미데이터 추가 완료" });
+  } catch (error) {
+    console.error(error);
+    next(error);
+  }
+});
+
+router.get("/add", async (req, res, next) => {
+  try {
+    Education.create({
+      title: "SQL-Injection",
+      category: 0,
+      page: 2,
+      content: "test입니다.&&test입니다.&&test입니다.",
+      image:
+        "https://secubook-img-data.s3.ap-northeast-2.amazonaws.com/Unknown.jpeg&&https://secubook-img-data.s3.ap-northeast-2.amazonaws.com/Unknown.jpeg",
+    });
+    res.send({ testMessage: "더미데이터 추가 완료" });
+  } catch (error) {
+    console.error(error);
+    next(error);
+  }
+});
+
+module.exports = router;
