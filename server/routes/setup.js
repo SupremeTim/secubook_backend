dd44afdbe23e9cc6654457b0aed5314241fd9253
InTaek <dlsxor21c@naver.com>
2020-10-28 22:50:59 +0900

    feat: 미니 문제 더미 데이터 추가

--- server/routes/setup.js
+++ server/routes/setup.js
@@ -1,5 +1,5 @@
 const express = require("express");
-const { Education } = require("../models");
+const { Education, Drill } = require("../models");
 
 const router = express.Router();
 
@@ -23,6 +23,15 @@ router.get("/", async (req, res, next) => {
         "https://secubook-img-data.s3.ap-northeast-2.amazonaws.com/Unknown.jpeg",
     });
 
+    Education.create({
+      title: "SQL-Injection",
+      category: 0,
+      page: 2,
+      content: "test입니다.&&test입니다.&&test입니다.",
+      image:
+        "https://secubook-img-data.s3.ap-northeast-2.amazonaws.com/Unknown.jpeg&&https://secubook-img-data.s3.ap-northeast-2.amazonaws.com/Unknown.jpeg",
+    });
+
     res.send({ testMessage: "더미데이터 추가 완료" });
   } catch (error) {
     console.error(error);
@@ -32,14 +41,16 @@ router.get("/", async (req, res, next) => {
 
 router.get("/add", async (req, res, next) => {
   try {
-    Education.create({
+    Drill.create({
       title: "SQL-Injection",
-      category: 0,
-      page: 2,
-      content: "test입니다.&&test입니다.&&test입니다.",
+      type: 0,
+      level: 0,
+      content: "test입니다.",
       image:
-        "https://secubook-img-data.s3.ap-northeast-2.amazonaws.com/Unknown.jpeg&&https://secubook-img-data.s3.ap-northeast-2.amazonaws.com/Unknown.jpeg",
+        "https://secubook-img-data.s3.ap-northeast-2.amazonaws.com/Unknown.jpeg",
+      answer: "정답입니다",
     });
+
     res.send({ testMessage: "더미데이터 추가 완료" });
   } catch (error) {
     console.error(error);
