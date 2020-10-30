31dc979e4d6ffb8edd8b4b576788f2e5f29437b6
InTaek <dlsxor21c@naver.com>
2020-10-30 11:31:16 +0900

    feat: 더미 데이터 추가

--- server/routes/setup.js
+++ server/routes/setup.js
@@ -1,5 +1,5 @@
 const express = require("express");
-const { Education, Drill, Board } = require("../models");
+const { User, Education, Drill, Board } = require("../models");
 
 const router = express.Router();
 
@@ -42,6 +42,16 @@ router.get("/", async (req, res, next) => {
       answer: "정답입니다",
     });
 
+    Board.create({
+      title: "궁금해요!",
+      content: "test입니다.",
+    });
+
+    Board.create({
+      title: "궁금해요2",
+      content: "test입니다.222",
+    });
+
     res.send({ testMessage: "더미데이터 추가 완료" });
   } catch (error) {
     console.error(error);
@@ -51,14 +61,24 @@ router.get("/", async (req, res, next) => {
 
 router.get("/add", async (req, res, next) => {
   try {
-    Board.create({
-      title: "궁금해요!",
-      content: "test입니다.",
+    User.create({
+      name: "조인택",
+      email: "dlsxor21c@naver.com",
+      password: "",
+      level: 0,
+      dockerPort: 80,
     });
 
     Board.create({
-      title: "궁금해요2",
-      content: "test입니다.222",
+      title: "궁금해요!4444",
+      content: "test입니다.44",
+      userId: 1,
+    });
+
+    Board.create({
+      title: "궁금해요555",
+      content: "test입니다.5555",
+      userId: 1,
     });
 
     res.send({ testMessage: "더미데이터 추가 완료" });
