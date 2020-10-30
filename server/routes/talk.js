d94c6a7b9aa615afb7714d58cf718ed6beb1fa0a
InTaek <dlsxor21c@naver.com>
2020-10-30 11:31:57 +0900

    feat: User 테이블과 join하고 content말고 id를 가져옴

--- server/routes/talk.js
+++ server/routes/talk.js
@@ -1,14 +1,18 @@
 const express = require("express");
 const { isLoggedIn, isNotLoggedIn } = require("./middlewares");
-const { Board } = require("../models");
+const { Board, User } = require("../models");
 
 const router = express.Router();
 
 router.get("/", async (req, res, next) => {
   try {
     const results = await Board.findAll({
-      attributes: ["title", "content"],
-      order: [["createdAt", "ASC"]],
+      include: {
+        model: User,
+        attributes: ["name"],
+      },
+      attributes: ["id", "title", "createdAt"],
+      order: [["createdAt", "DESC"]],
     });
 
     res.send({
@@ -21,5 +25,9 @@ router.get("/", async (req, res, next) => {
     res.status(500).send({ errorMessage: "서버 내부 오류입니다." });
   }
 });
+
+// 세부내용 라우터 추가
+
+// 게시판 추가 라우터 추가
 
 module.exports = router;
