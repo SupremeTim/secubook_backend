54e9daa52597e9acbd1c7beb5edc8fbe19653ce3
InTaek <dlsxor21c@naver.com>
2020-11-01 22:15:21 +0900

    feat: codingTest 디비 수정

--- server/models/codingTest.js
+++ server/models/codingTest.js
@@ -1,3 +1,5 @@
+const { all } = require("sequelize/types/lib/operators");
+
 module.exports = (sequelize, DataTypes) =>
   sequelize.define(
     "codingTest",
@@ -10,9 +12,27 @@ module.exports = (sequelize, DataTypes) =>
         type: DataTypes.STRING(500),
         allowNull: false,
       },
+      category: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+      },
+      level: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+      },
       image: {
         type: DataTypes.STRING(200),
+        allowNull: true,
+      },
+      cntOfSolve: {
+        type: DataTypes.INTEGER,
         allowNull: false,
+        defaultValue: 0,
+      },
+      cntOfRun: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+        defaultValue: 0,
       },
       timeAverage: {
         type: DataTypes.INTEGER,
