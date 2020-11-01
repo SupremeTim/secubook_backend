a2daacd8ef7ea1cdf2f605c21589f44f8353cd1a
InTaek <dlsxor21c@naver.com>
2020-11-02 00:55:39 +0900

    feat: 게시물 상세 보기 라우터 추가

--- server/routes/talk.js
+++ server/routes/talk.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const { isLoggedIn, isNotLoggedIn } = require("./middlewares");
-const { Board, User } = require("../models");
+const { Board, User, Comment } = require("../models");
 
 const router = express.Router();
 
@@ -26,7 +26,30 @@ router.get("/", async (req, res, next) => {
   }
 });
 
-// 세부내용 라우터 추가
+// 로그인 미들웨어 추가 필요
+router.get("/detail/:id", async (req, res, next) => {
+  try {
+    const results = await Board.findOne({
+      include: {
+        model: Comment,
+        attributes: ["host", "content", "createdAt"],
+      },
+      attributes: ["title", "content", "createdAt"],
+      where: {
+        id: req.params.id,
+      },
+    });
+
+    res.send({
+      user: req.user,
+      results: results,
+      testMessage: "선택 게시판 내용과 댓글을 불러왔습니다.",
+    });
+  } catch (error) {
+    console.error(error);
+    res.status(500).send({ errorMessage: "서버 내부 오류입니다." });
+  }
+});
 
 // 게시판 추가 라우터 추가
 
