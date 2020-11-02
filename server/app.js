bd5ebf38c736eac3d87b58e9d789f3ab08fc0cf4
InTaek <dlsxor21c@naver.com>
2020-11-02 23:02:06 +0900

    feat: 헤더 정보 추가

--- server/app.js
+++ server/app.js
@@ -89,6 +89,15 @@ app.use(
     credentials: true,
   })
 ); // https://velog.io/@bigbrothershin/Backend-다른-도메인-간에-쿠키-주고받기
+app.use(function (req, res, next) {
+  res.header("Content-Type", "application/json;charset=UTF-8");
+  res.header("Access-Control-Allow-Credentials", true);
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
+  next();
+});
 app.use(session(sessionOption));
 app.use(flash());
 app.use(passport.initialize());
