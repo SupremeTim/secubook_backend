9833eed2664617c69bf059c1426b3fcabcc2e01a
InTaek <dlsxor21c@naver.com>
2020-11-02 14:04:50 +0900

    fix: Add aync

--- server/routes/problem.js
+++ server/routes/problem.js
@@ -4,19 +4,32 @@ const { CodingTest } = require("../models");
 
 const router = express.Router();
 
-router.get("/", (req, res, next) => {
-  const results = CodingTest.findAll({
-    attributes: [
-      "id",
-      "title",
-      "category",
-      "level",
-      "cntOfSolve",
-      "cntOfRun",
-      "timeAverage",
-    ],
-  });
-  res.send({ user: req.user, testMessage: "코딩 테스트 페이지에 왔습니다." });
+router.get("/", async (req, res, next) => {
+  try {
+    const results = await CodingTest.findAll({
+      attributes: [
+        "id",
+        "title",
+        "category",
+        "level",
+        "cntOfSolve",
+        "cntOfRun",
+        "timeAverage",
+      ],
+    });
+    res.send({
+      user: req.user,
+      results: results,
+      testMessage: "코딩 테스트 페이지에 왔습니다.",
+    });
+  } catch (error) {
+    console.error(error);
+    res.status(500).send({ errorMessage: "서버 내부 오류" });
+  }
 });
+
+// 문제 클릭 라우터
+
+// 문제 채점 라우터
 
 module.exports = router;
