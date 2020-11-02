d4be366c9351f3b41d78c3cb2691650eb166da06
InTaek <dlsxor21c@naver.com>
2020-11-02 15:23:18 +0900

    fix: 테스트 메세지 추가

--- server/routes/level.js
+++ server/routes/level.js
@@ -4,8 +4,13 @@ const { Drill } = require("../models");
 
 const router = express.Router();
 
+// 레벨 테스트 페이지
 router.get("/", (req, res, next) => {
   res.send({ user: req.user, testMessage: "레벨 테스트 페이지에 왔습니다." });
 });
+
+// 레벨 테스트 문제 클릭
+
+// 레벨 테스트 정답 확인
 
 module.exports = router;
