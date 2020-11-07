1b68bb3ba6493292db33584e80d85a1d674a3d56
InTaek <dlsxor21c@naver.com>
2020-11-07 21:24:40 +0900

    feat: 롤백

--- server/app.js
+++ server/app.js
@@ -8,7 +8,6 @@ const passport = require("passport");
 const helmet = require("helmet");
 const hpp = require("hpp");
 const cors = require("cors");
-const FileStore = require("session-file-store")(session);
 require("dotenv").config();
 
 // Router
@@ -53,7 +52,6 @@ const sessionOption = {
     httpOnly: true,
     secure: false,
   },
-  store: new FileStore(),
 };
 
 app.use(
