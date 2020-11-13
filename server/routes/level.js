5e51f8ca47d5f5b5e45e1ab5b7614de47e8c44e7
InTaek <dlsxor21c@naver.com>
2020-11-13 22:33:13 +0900

    feat: 레벨테스트 정답 확인 라우터 추가

--- server/routes/level.js
+++ server/routes/level.js
@@ -28,5 +28,50 @@ router.post("/test", isLoggedIn, async (req, res, next) => {
 });
 
 // 레벨 테스트 정답 확인
+router.post("/check", isLoggedIn, async (req, res, next) => {
+  const { userAnswer, level } = req.body;
+
+  try {
+    const result = await Drill.findAll({
+      where: {
+        level: level,
+      },
+      attributes: ["answer"],
+    });
+
+    // res.send({ r: result });
+    for (let index = 0; index < userAnswer.length; index++) {
+      const element1 = userAnswer[index];
+      const element2 = result[index].answer;
+
+      if (element1 != element2) {
+        return res.status(200).send({
+          result: "틀렸습니다.",
+        });
+      }
+    }
+
+    if (req.user.level < level) {
+      await User.update(
+        {
+          level: level,
+        },
+        {
+          where: {
+            id: req.user.id,
+          },
+        }
+      );
+    }
+
+    return res.send({
+      result: "모두 맞았습니다.",
+      testMessage: "유저의 레벨을 업데이트했습니다.",
+    });
+  } catch (error) {
+    console.error(error);
+    res.status(500).send({ errorMessage: "서버 내부 오류입니다." });
+  }
+});
 
 module.exports = router;
